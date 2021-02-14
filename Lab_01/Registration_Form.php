6465050969e94f945028c4eba8a7dce1d12c3808
Afridi Mozumder <49424469+afridimozumder@users.noreply.github.com>
2021-02-14 16:29:38 +0600

    Add files via upload

---
+++ Lab_01/Registration_Form.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> ~ Registration Page ~ </title>
+</head>
+
+<body>
+
+<h1> Registration Page </h1>
+
+<form>
+<table>
+
+<tr>
+<td>First Name</td>
+<td>:<input type="text" name="firstname" placeholder="Enter Your First Name"></td>
+</tr>
+<tr>
+<td>Email</td>
+<td>:<input type="text" name="mail" placeholder="Enter Valid Email"></td>
+</tr>
+<tr>
+<td>Username    </td>
+<td>:<input type="text" name="uname" placeholder="Enter Username"></td>
+</tr>
+<tr>
+<td>Password</td>
+<td>:<input type="password" name="password" placeholder="Enter Password"></td>
+</tr>
+<tr>
+<td>Confirm Password</td>
+<td>:<input type="password" name="password" placeholder="Enter Password Again"></td>
+</tr>
+
+<tr>
+<td>Gender</td>
+</tr>
+<tr>
+<td><input type="radio" name="gender">Male</td>
+<td><input type="radio" name="gender">Female</td>
+<td><input type="radio" name="gender">Other</td>
+</tr>
+</table>
+<tr>
+<td>Date Of Birth<br></td>
+</tr>
+<tr>
+<td><input type="date" name="date"></td>
+</tr>
+
+<tr>
+<td><input type="submit" name="Submit"></td>
+</tr>
+<tr>
+<td><input type="reset" name="Reset"></td>
+</tr>
+
+</form>
+
+</body>
+<html>
